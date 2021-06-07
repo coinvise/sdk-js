6ad9e3995ee15c01d9a2c57446ce42514d183478
Mihir Gupta <mihirgupta0900@gmail.com>
2021-06-07 16:31:06 +0530

    added enpoints and methods

--- src/coinvise.ts
+++ src/coinvise.ts
@@ -6,11 +6,15 @@ import {
   changeWebhookUrl,
   ChangeWebhookUrlParams,
   ChangeWebhookUrlResponse,
+  generateWebhookSecret,
+  GenerateWebhookSecretResponse,
   mintToken,
   MintTokenParams,
   MintTokenResponse,
+  VerifyWebhookParams,
 } from './api-endpoints';
 import { buildRequestError, HTTPResponseError } from './errors';
+import { computeSignature, parseHeader, secureCompare } from './helpers';
 import { AuthHeaders } from './interfaces/auth.interface';
 import {
   ClientOptions,
@@ -56,7 +60,7 @@ export default class Coinvise {
         'user-agent': 'coinvise-client/0.1.0',
       },
       retry: 0,
-      agent: makeAgentOption(prefixUrl, options?.agent),
+      // agent: makeAgentOption(prefixUrl, options?.agent),
     });
   }
 
@@ -91,8 +95,8 @@ export default class Coinvise {
 
       const response = await this.#got(path, {
         method,
-        searchParams: query,
-        json,
+        ...(query && { searchParams: query }),
+        ...(json && { json }),
         headers,
       }).json<Response>();
 
@@ -146,6 +150,97 @@ export default class Coinvise {
         method: changeWebhookUrl.method,
         body: args,
       });
+    },
+
+    /**
+     * Generates webhook secret that is used to verify the integrity of the data sent from Coinvise
+     */
+    generateSecret: async (): Promise<string> => {
+      const res = await this.request<GenerateWebhookSecretResponse>({
+        path: generateWebhookSecret.path(),
+        method: generateWebhookSecret.method,
+      });
+      return res.data.secret;
+    },
+
+    /**
+     * Verifies webhook from the given body and signature
+     */
+    verify: (args: VerifyWebhookParams): boolean => {
+      const payload = Buffer.isBuffer(args.payload)
+        ? args.payload.toString('utf8')
+        : args.payload;
+
+      // Express's type for `Request#headers` is `string | []string`
+      // which is because the `set-cookie` header is an array,
+      // but no other headers are an array (docs: https://nodejs.org/api/http.html#http_message_headers)
+      // (Express's Request class is an extension of http.IncomingMessage, and doesn't appear to be relevantly modified: https://github.com/expressjs/express/blob/master/lib/request.js#L31)
+      if (Array.isArray(args.header)) {
+        throw new Error(
+          'Unexpected: An array was passed as a header, which should not be possible for the stripe-signature header.'
+        );
+      }
+
+      const header = Buffer.isBuffer(args.header)
+        ? args.header.toString('utf8')
+        : args.header;
+
+      const details = parseHeader(header);
+
+      if (!details || details.timestamp === -1) {
+        this.log(
+          LogLevel.WARN,
+          'Unable to extract timestamp and signatures from header',
+          {
+            header,
+            payload,
+          }
+        );
+        return false;
+      }
+
+      if (!details.signature) {
+        this.log(LogLevel.WARN, 'No signatures found', {
+          header,
+          payload,
+        });
+        return false;
+      }
+
+      const expectedSignature = computeSignature(
+        `${details.timestamp}.${payload}`,
+        args.secret
+      );
+
+      const signatureFound = secureCompare(
+        details.signature,
+        expectedSignature
+      );
+
+      if (!signatureFound) {
+        this.log(
+          LogLevel.WARN,
+          'No signatures found matching the expected signature for payload',
+          {
+            header,
+            payload,
+          }
+        );
+        return false;
+      }
+
+      const timestampAge = Math.floor(Date.now() / 1000) - details.timestamp;
+
+      const tolerance = args.tolerance ?? 18000;
+      if (tolerance > 0 && timestampAge > tolerance) {
+        this.log(LogLevel.WARN, 'Timestamp outside the tolerance zone', {
+          header,
+          payload,
+        });
+        return false;
+      }
+
+      return true;
     },
   };
 
